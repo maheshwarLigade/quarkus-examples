ab802d44abfcc9a9b651e8c2aa182cc9899e9bf9
maheshwarligade <maheshwar.ligade@gmail.com>
2022-06-22 19:42:52 +0530

    helloworld example

---
+++ kotlin-quarkus-hello/src/native-test/java/org/techwasti/demo/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.techwasti.demo;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
